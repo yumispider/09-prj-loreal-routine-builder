a29296a62b6a6b8740853b366c8b0495b29ca98b
yumispider <yumispider@gmail.com>
2026-04-12 18:54:42 +0000

    Added basic selection functionality

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const SELECTED_PREFIX = "selected-";
+const PRODUCT_ID_PREFIX = "product-";
+
 /* Get references to DOM elements */
 const categoryFilter = document.getElementById("categoryFilter");
 const productsContainer = document.getElementById("productsContainer");
 const chatForm = document.getElementById("chatForm");
 const chatWindow = document.getElementById("chatWindow");
+const selectedProducts = document.getElementById("selectedProductsList");
+
+/* Array of products that are selected */
+const selectedProductsList = [];
 
 /* Show initial placeholder until user selects a category */
 productsContainer.innerHTML = `
@@ -18,21 +25,104 @@ async function loadProducts() {
   return data.products;
 }
 
-/* Create HTML for displaying product cards */
-function displayProducts(products) {
-  productsContainer.innerHTML = products
-    .map(
-      (product) => `
-    <div class="product-card">
+/* Extracts the id from the DOM id of a product */
+function separateID(id) {
+  const stringID = "" + id;
+  const cutIndex =
+    stringID.indexOf(PRODUCT_ID_PREFIX) + PRODUCT_ID_PREFIX.length;
+  const splitID = stringID.substring(cutIndex);
+  return splitID;
+}
+
+/* Locates a selected item using its ID in the document and returns the index
+ * (ex. selected-product-2)
+ */
+function locateItemByID(id, productsList) {
+  const targetID = separateID(id);
+  let found = false;
+  let i = 0;
+  while (i < productsList.length && !found) {
+    const currentProduct = productsList[i];
+    const currentID = currentProduct.id;
+    found = currentID == targetID;
+    if (!found) {
+      i++;
+    }
+  }
+  if (!found) {
+    i = -1;
+    console.log(`WARNING: Couldn't locate product by ID ${targetID}`);
+  }
+  return i;
+}
+
+/* Unselect a product */
+function removeFromSelectedItems(selectedProductID) {
+  const removeIndex = locateItemByID(selectedProductID, selectedProductsList);
+  selectedProductsList.splice(removeIndex, 1);
+  refreshSelectedItems();
+}
+
+/* Select a product to include in the generated routine */
+function addToSelectedItems(selectedProduct) {
+  selectedProductsList.push(selectedProduct);
+  refreshSelectedItems();
+}
+
+/* Refresh the products selected in the box */
+function refreshSelectedItems() {
+  selectedProducts.innerHTML = "";
+  for (let i = 0; i < selectedProductsList.length; i++) {
+    const product = selectedProductsList[i];
+    selectedProducts.innerHTML += `
+    <div class="selected-product-card" id="${SELECTED_PREFIX}${PRODUCT_ID_PREFIX}${product.id}">
       <img src="${product.image}" alt="${product.name}">
       <div class="product-info">
         <h3>${product.name}</h3>
         <p>${product.brand}</p>
       </div>
     </div>
-  `
+  `;
+  }
+
+  const selectedProductsQuery = document.querySelectorAll(
+    ".selected-product-card",
+  );
+
+  selectedProductsQuery.forEach((cur) => {
+    console.log(cur);
+  });
+}
+
+/* Create HTML for displaying product cards */
+function displayProducts(products) {
+  productsContainer.innerHTML = products
+    .map(
+      (product) => `
+    <div class="product-card" id="${PRODUCT_ID_PREFIX}${product.id}">
+      <img src="${product.image}" alt="${product.name}">
+      <div class="product-info">
+        <h3>${product.name}</h3>
+        <p>${product.brand}</p>
+      </div>
+    </div>
+  `,
     )
     .join("");
+  products.forEach((cur) => {
+    const curProduct = document.getElementById(`${PRODUCT_ID_PREFIX}${cur.id}`);
+
+    curProduct.addEventListener("click", async (e) => {
+      const products = await loadProducts();
+      const targetID = e.target.closest(".product-card").id;
+      console.log(targetID);
+
+      const targetIDIndex = locateItemByID(targetID, products);
+      const targetProduct = products[targetIDIndex];
+
+      addToSelectedItems(targetProduct);
+    });
+  });
 }
 
 /* Filter and display products when category changes */
@@ -43,7 +133,7 @@ categoryFilter.addEventListener("change", async (e) => {
   /* filter() creates a new array containing only products 
      where the category matches what the user selected */
   const filteredProducts = products.filter(
-    (product) => product.category === selectedCategory
+    (product) => product.category === selectedCategory,
   );
 
   displayProducts(filteredProducts);
